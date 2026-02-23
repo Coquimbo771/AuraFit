c38dc2d2839f201662e914ce573fa259af642f38
AnderesLapt <andres10000fky@gmail.com>
2026-02-23 10:21:51 -0400

    Pull Anderes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, Suspense, lazy } from 'react';
+import { useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthStore } from './store/authStore';
 import { Navigation } from './components/Navigation';
